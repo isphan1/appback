5c333cd7b1b31dea6485524b35b0e7d7f6f434ce
KILLER <isphan111@gmail.com>
2020-12-28 20:19:25 +0600

    apilz

--- chat/views.py
+++ chat/views.py
@@ -7,25 +7,6 @@ from chat.models import Profile,Messages
 def index(request):
 
     friends = Profile.objects.filter(user_id=2)
-    list = []
-
-    for c in friends:
-        m = []
-
-        msg1 = Messages.objects.filter(user_m=2).filter(friend_m=c.friend_id.id).order_by('created_at')
-        msg2 = Messages.objects.filter(user_m=c.friend_id.id).filter(friend_m=2).order_by('created_at')
-
-        for x in msg1:
-                m.append(x)
-        for x in msg2:
-                m.append(x)
-            
-        def myFunc(e):
-            return e.created_at
-
-        m.sort(key=myFunc)
-
-        list.append(m)
             
 
-    return render(request,'index.html',{'list':list})
+    return render(request,'index.html',{'list':friends})
